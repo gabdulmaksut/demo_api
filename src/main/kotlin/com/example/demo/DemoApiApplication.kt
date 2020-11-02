4a6c952037bc35d1bc12eb9bb908fc261aff43df
agabdulmaksut <Yabloko96!>
2020-11-02 20:37:34 +0600

    application

--- src/main/kotlin/com/example/demo/DemoApiApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApiApplication.kt
@@ -2,17 +2,14 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 
-@SpringBootApplication
-@ComponentScan
-@EntityScan
+@SpringBootApplication( exclude = [DataSourceAutoConfiguration::class] )
 @EnableJpaRepositories
-
 class DemoApiApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/example/demo/todo/TodoRepository.kt
+++ src/main/kotlin/com/example/demo/todo/TodoRepository.kt
@@ -3,5 +3,5 @@ package com.example.demo.todo
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-@Repository
+
 interface TodoRepository : JpaRepository<Todo, Long>{}
